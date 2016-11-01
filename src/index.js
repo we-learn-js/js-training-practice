972ea71426601272c4ae8301726f47854931013e
Carles Trullenque <carlestf@hotmail.com>
2016-11-01 21:11:21 +0100

    convirtiendo en expresiones

--- src/index.js
+++ src/index.js
@@ -48,11 +48,8 @@ var quiz = function (element, options) {
   }
 
   function getMultipleChoiceField (type, name, idx, label, checked) {
-    if (checked) {
-      checked = 'checked'
-    } else {
-      checked = ''
-    }
+    checked=checked ? 'checked' : ''
+
     return '<div class="field">'
     + '<div class="ui checkbox ' + type + '">'
     + '<input type="' + type + '" ' + checked + ' name="' + name + '" id="' + name + '_' + idx + '" value="' + label + '">'
@@ -173,24 +170,14 @@ var quiz = function (element, options) {
   }
 
   function isEmptyResponse (response) {
-    if(!response) {
-      return true
-    }
-    if(response.join) {
-      if (!response.join('')) {
-        return true
-      }
-    }
-    return false
+
+    return !response || (response.join && !response.join('')) || false
   }
 
   function getResponseCount (responses) {
     return responses.reduce(function (result, response) {
-      if (isEmptyResponse(response)) {
-        return result
-      } else {
-        return result + 1
-      }
+      return (isEmptyResponse(response))?result:result+1
+
     }, 0)
   }
 
@@ -229,11 +216,7 @@ var quiz = function (element, options) {
 
       getQuizResponse(currentQuestion)
         .then(function (correctResponse) {
-          if (isResponseCorrect(response, correctResponse)) {
-            alert('Response is correct!')
-          } else {
-            alert('Response is not correct! It was: ' + serializeResponse(correctResponse))
-          }
+          isResponseCorrect(response, correctResponse) ? alert('Response is correct!') : alert('Response is not correct! It was: ' + serializeResponse(correctResponse))
 
           updateQuizStatus(questions, responseCount)
           saveQuizData({
@@ -250,20 +233,14 @@ var quiz = function (element, options) {
   }
 
   function serializeResponse (response) {
-    if (response.join) {
-      return response.sort().join(', ')
-    } else {
-      return response
-    }
+    return response.join ? response.sort().join(', ') : response;
   }
 
   function updateQuizStatus (questions, responseCount) {
     showCurrentQuestion(responseCount)
     updateProgressBar(questions.length, responseCount)
 
-    if (questions.length === responseCount) {
-      showTextEndMessage()
-    }
+    questions.length === responseCount && showTextEndMessage()
   }
 
   function saveQuizData (changes) {
