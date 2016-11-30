5988f15528e67b6321fcaba5f548f3fd5b2e890d
Chema <jose.rodriguez@scmspain.com>
2016-11-30 14:59:02 +0100

    create base class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,80 @@
 
-UserQuiz={
+class UserQuiz{
+  constructor() {
+    this.userResponses=loadResponses();
+    this.responses = this.userResponses.responses || [];
+    this.currentQuestion = this.userResponses.currentQuestion || 0;
+    this.responseCount = this.userResponses.responseCount;
+  }
+  
+  loadResponses(){
+    storedData = localStorage.getItem('quiz')
+    return (storedData) ? JSON.parse(storedData) : {}
+  }
+  
+  processResponse ($questions, questions) {
+    var currentQuestion = this.currentQuestion
+    var response = getQuestionResponse(questions[currentQuestion], currentQuestion)
+    var responses = this.responses
+    responses[currentQuestion] = response
 
-};
+    if ( isEmptyResponse(responses[currentQuestion]) ) {
+      alert('You must give a response')
+    } else {
+      var responseCount = getResponseCount(responses)
 
-Questions={
-  //(desc, options, isCorrect)
+      getQuizResponse(currentQuestion, function(correctResponse){
+        if( isResponseCorrect(response, correctResponse) ) {
+          alert('Response is correct!')
+        } else {
+          alert('Response is not correct! It was: ' + serializeResponse(correctResponse) )
+        }
+
+        updateQuizStatus($questions, questions, responseCount)
+        this.saveQuizData()
+      })
+    }
+  }
+  
+  saveQuizData() {
+    changes={
+          responses: this.responses,
+          responseCount: this.responseCount,
+          currentQuestion: ++this.currentQuestion
+        };
+    quizData = Object.assign(getQuizData(), changes)
+    localStorage.setItem('quiz', JSON.stringify(quizData))
+  }
+  
   
 };
 
-QuizNav={};
+class Questions {
+  //(desc, options, isCorrect)
+  constructor() {
+    this.questions=this.getQuestions();
+  }
+  
+  getQuestions(){
+    questions=[];
+    //Load questions of AJAX
+    return questions;
+  }
+  
+  displayQuestion(){
+    //
+  }
+}
 
-QuizApi={};
+class QuizNav{
+  constructor(currentQuestion=0) {
+    this.currentQuestion=currentQuestion;
+  }
+}
+
+class QuizApi{
+  
+}
 
 
 quiz = function (element, options) {
