03e4a789b58b27c737c69493a999c4b69516959c
Francisco de la Casa <francisco.delacasa@scmspain.com>
2017-01-20 12:18:26 +0100

    Strategy Pattern in Question Class

--- src/lib/Question.js
+++ src/lib/Question.js
@@ -2,43 +2,49 @@
  * Strategy Pattern
  * Responsible for question info and validation
  */
-// class Question {
-//   /**
-//    * @param  {Object} props Properties of the question
-//    */
-//   constructor(props) {}
-//
-//   /**
-//    * Get statement of the question
-//    * @return {String}
-//    */
-//   getStatement() {}
-//
-//   /**
-//    * Validate is response is correct for the question
-//    * @param  {String|Array} response
-//    * @return {Promise}
-//    */
-//   isResponseCorrect(response) {}
-//
-//   /**
-//    * Get is response if empty
-//    * @param  {String|Array} response
-//    * @return {Boolean}
-//    */
-//   isResponseEmpty(response) {}
-// }
 
 import QuizApi from './QuizApi'
 
-export default class Question {
+class QuestionValidationStrategy {
+  selectResponseCorrectValidator (validator) {
+    this._responseCorrectvalidator = validator;
+  }
+  selectIsResponseEmptyValidator (validator) {
+    this._isResponseEmptyvalidator = validator;
+  }
+
+  isResponseCorrect(value) {
+    this._responseCorrectvalidator.isResponseCorrect(value);
+  }
+
+  isResponseEmpty(value) {
+    this._isResponseEmptyvalidator.isResponseEmpty(value);
+  }
+}
+
+class Input extends InputValidationStrategy {
+  setElement(DOMElement) {}
+  getValue () {}
+  setValue () {}
+  /* ... */
+}
+class InputEmail {
+  constructor () {
+    super()
+    this.selectValidator(emailValidator)
+  }
+}
+class InputPhone {
+  constructor () {
+    super()
+    this.selectValidator(phoneValidator)
+  }
+}
+
+class Question extends QuestionValidationStrategy {
 
   static serializeResponse (response) {
     return (response.join && response.sort().join(', ')) || response
-  }
-
-  static isEmptyResponse (response) {
-    return !response || (response.join && !response.join('')) || false
   }
 
   constructor (id, {problem, input} = {}) {
@@ -46,7 +52,9 @@ export default class Question {
     this.__problem__ = problem
     this.__type__ = input && input.type
   }
+}
 
+const isResponseCorrectTextValidator = {
   isResponseCorrect (response) {
     return QuizApi.getResponse(this.__id__)
       .then(Question.serializeResponse)
@@ -55,6 +63,65 @@ export default class Question {
           ok: correctResponse == Question.serializeResponse(response),
           correctResponse: correctResponse
         }
-      } )
+      })
+}
+
+const isResponseEmptyTextValidator = {
+  isEmptyResponse (response) {
+    return !response || (response.join && !response.join('')) || false
   }
 }
+
+const isResponseCorrectRadioValidator = {
+  isResponseCorrect (response) {
+    return QuizApi.getResponse(this.__id__)
+      .then(Question.serializeResponse)
+      .then(function(correctResponse) {
+        return {
+          ok: correctResponse == Question.serializeResponse(response),
+          correctResponse: correctResponse
+        }
+      })
+}
+
+const isResponseEmptyRadioValidator = {
+  isEmptyResponse (response) {
+    return !response || (response.join && !response.join('')) || false
+  }
+}
+
+
+const isResponseCorrectCheckboxValidator = {
+  isResponseCorrect (response) {
+    return QuizApi.getResponse(this.__id__)
+      .then(Question.serializeResponse)
+      .then(function(correctResponse) {
+        return {
+          ok: correctResponse == Question.serializeResponse(response),
+          correctResponse: correctResponse
+        }
+      })
+}
+
+const isResponseEmptyCheckboxValidator = {
+  isEmptyResponse (response) {
+    return !response || (response.join && !response.join('')) || false
+  }
+}
+
+class TextQuestion extends Question {
+  this.selectResponseCorrectValidator(isResponseCorrectTextValidator);
+  this.selectIsResponseEmptyValidator(isResponseEmptyTextValidator);
+}
+
+class RadioQuestion extends Question {
+  this.selectResponseCorrectValidator(isResponseCorrectRadioValidator);
+  this.selectIsResponseEmptyValidator(isResponseEmptyRadioValidator);
+}
+
+class CheckboxQuestion extends Question {
+  this.selectResponseCorrectValidator(isResponseCorrectCheckboxValidator);
+  this.selectIsResponseEmptyValidator(isResponseEmptyCheckboxValidator);
+}
+
+export { TextQuestion, RadioQuestion, CheckboxQuestion }
