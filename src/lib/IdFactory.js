6d180a0c56a7890e86019886b451c820609e740c
Francisco de la Casa <francisco.delacasa@scmspain.com>
2017-01-31 23:09:07 +0100

    DOM and IdFactory

--- src/lib/IdFactory.js
+++ src/lib/IdFactory.js
@@ -4,13 +4,14 @@ var count = 0
  * In charge of unique ids creation
  */
 class IdFactory {
-  
+  this._id = 0;
+
   /**
    * Get unique id. Used for Html elements identification.
    * @return {String}
    */
   static getId () {
-
+    return `id_${this._id++}`;
   }
 }
 
