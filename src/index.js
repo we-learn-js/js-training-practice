04bed8a55e074ea7d4be1badb4da4bedadcbaf75
David Almeida <davidbarna@gmail.com>
2017-10-19 17:29:15 +0200

    Add comments to code

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ $.ajax({
 }).done(function (data) {
   questions = data.questions
 
+  // Load data from past reponses
   try {
     quizData = JSON.parse(localStorage.getItem('quiz'))
     responses = quizData.responses || []
@@ -20,27 +21,29 @@ $.ajax({
   }
 
 
+  // Append the progress bar to DOM
   $('body')
     .append('<div style="position: fixed; bottom: 0; background: #eee; width: 100%; height: 6px; ">'
       + '<div id="progress" style="background: #1678c2; width: 1%;">&nbsp;</div>'
       + '</div>')
+
+  // Append title and form to quiz
   $('#quiz')
     .append('<h1 class="ui header">' + data.title + '</h1>')
     .append('<form id="quiz-form" class="ui form"></form>')
 
+  // For each question of the json,
   for (var i = 0; i < data.questions.length; i++) {
     question = data.questions[i]
-
-    if (question.code !== undefined) {
-      var code = '<pre><code>' + question.code + '</code></pre>'
-    } else {
-      var code = ''
-    }
 
     if (question.input === undefined) {
       question.input = { type: 'input' }
     }
+
+    // Construct the input depending on question type
     switch (question.input.type) {
+
+      // Multiple options
       case 'checkbox':
       case 'radio':
         var input = '<div class="inline fields">'
@@ -64,6 +67,7 @@ $.ajax({
         input += '</div>'
         break
 
+      // Set of inputs (composed response)
       case 'inputs':
         var input = '<table>'
         for (j = 0; j < question.input.options.length; j++) {
@@ -87,6 +91,8 @@ $.ajax({
         }
         input += '</table>'
         break
+
+      // Default: simple input
       default:
         if (!!responses[i]) {
           var value = responses[i]
@@ -103,43 +109,49 @@ $.ajax({
       + '<div class="header">' + question.problem + '</div>'
       + '</div>'
       + '<div class="content">'
-      + code
-      + '</div>'
-      + '<div class="content">'
       + input
       + '</div>'
       + '</div>'
     ).css('display', 'none')
 
-    $('#quiz-form').append($question)
+    $('#quiz-form')
+      .append($question)
 
-    $('pre code').each(function (i, block) {
-      hljs.highlightBlock(block)
-    })
+    // Show current question
+    $('#quiz-form')
+      .find('#question-' + currentQuestion)
+      .css('display', 'block')
 
-    $('#quiz-form').find('#question-' + currentQuestion).css('display', 'block')
-    $('#progress').css('width', (responseCount / questions.length * 100) + '%')
+    // Update progress bar
+    $('#progress')
+      .css('width', (responseCount / questions.length * 100) + '%')
   }
-  $('#quiz').append('<button id="submit-response" class="ui primary button">Submit response</button>')
 
+  // Add button to submit response
+  $('#quiz')
+    .append('<button id="submit-response" class="ui primary button">Submit response</button>')
+
+  // Is case all questions have been responded
   if (responseCount === questions.length) {
     $('#submit-response').css('display', 'none')
     $('#quiz').append('<div>Thank you for your responses.<br /><br /> </div>')
     $('#quiz').append('<button class="ui primary button" onclick="window.print()" >Print responses</button>')
   }
 
+  // Add a reset button that will redirect to quiz start
   $resetButton = $('<button class="ui button negative">Reset</button>')
   $resetButton.on('click', function(){
     localStorage.removeItem('quiz')
     location.reload();
   })
-
   $('#quiz').append($resetButton)
 
+  // Actions on every response submission
   $('#submit-response').on('click', function () {
     var $inputs = $('[name^=question_' + currentQuestion + ']')
     var question = questions[currentQuestion]
 
+    // Behavior for each question type to add response to array of responses
     switch (question.input.type) {
       case 'checkbox':
       case 'radio':
@@ -161,6 +173,7 @@ $.ajax({
         responses[currentQuestion] = $inputs.val()
     }
 
+    // Set the current responses counter
     var responseCount = 0
     for (i = 0; i < responses.length; i++) {
       question = questions[i]
@@ -179,14 +192,15 @@ $.ajax({
       }
     }
 
-    $('#progress').css('width', (responseCount / questions.length * 100) + '%')
+    // Update progress bar
+    $('#progress')
+      .css('width', (responseCount / questions.length * 100) + '%')
 
+    // Check if question had a valid answer
     isQuestionAnswered = true
-
     if (!responses[currentQuestion]) {
       isQuestionAnswered = false
     }
-
     if (!!responses[currentQuestion] && !!responses[currentQuestion].length) {
       for (j = 0; j < responses[currentQuestion].length; j++) {
         if (!responses[currentQuestion][j]) {
@@ -196,12 +210,19 @@ $.ajax({
     }
 
     if (!isQuestionAnswered) {
+      // Alert user of missing response
       alert('You must give a response')
     } else {
-      $('#quiz-form').find('#question-' + currentQuestion).css('display', 'none')
-      currentQuestion = currentQuestion + 1
-      $('#quiz-form').find('#question-' + currentQuestion).css('display', 'block')
 
+      // Display next question
+      $('#quiz-form')
+        .find('#question-' + currentQuestion).css('display', 'none')
+      currentQuestion = currentQuestion + 1
+
+      $('#quiz-form')
+        .find('#question-' + currentQuestion).css('display', 'block')
+
+      // If it was the las question, display final message
       if (responseCount === questions.length) {
         $('#submit-response').css('display', 'none')
         $('#quiz').append('<div>Thank you for your responses.<br /><br /> </div>')
@@ -209,6 +230,7 @@ $.ajax({
       }
     }
 
+    // Save current state of the quiz
     quizData.responses = responses
     quizData.responseCount = responseCount
     quizData.currentQuestion = currentQuestion
