725c0fb8f4ac807e13a98b65c42ccbe442f42c05
Irene Zeller <irene.zeller@infojobs.net>
2017-11-02 15:21:00 +0100

    Add destructuring for QuizData

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-responseCount = 0
-currentQuestion = 0
+
 options = {
   url: 'data/quiz.json?' + Date.now()
 }
@@ -8,22 +7,13 @@ $.ajax({
   url: options.url
 }).done(function(data) {
   questions = data.questions
-
+  let quizData
   // Load data from past reponses
   try {
-    quizData = JSON.parse(localStorage.getItem('quiz'))
-    responses = quizData.responses || []
-    currentQuestion = quizData.currentQuestion || -1
-    responseCount = quizData.responseCount || -1
+    quizData = JSON.parse(localStorage.getItem('quiz')) || {}
   } catch (e) {}
 
-  if (quizData == null) {
-    quizData = {
-      responses: []
-    }
-    responses = quizData.responses
-  }
-
+  let {responses = [], currentQuestion = 0, responseCount = 0} = quizData
 
   // Append the progress bar to DOM
   $('body')
